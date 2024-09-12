6a77b9547aae1ece9bb136b1ac33e6c28cc5a26d
DivyaBinduMenda <129891690+DivyaBinduMenda@users.noreply.github.com>
2024-09-12 23:05:23 +0530

    Update ProjectEchoStreamApplicationTests.java

--- src/test/java/com/example/tunehub/ProjectTuneHubApplicationTests.java
+++ src/test/java/com/example/tunehub/ProjectTuneHubApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProjectTuneHubApplicationTests {
+class ProjectEchoStreamApplicationTests {
 
 	@Test
 	void contextLoads() {
